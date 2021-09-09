5b3b0cc9401aed53de023be0b7a6e5977f5c62a5
Godam-Lee <85333656+Godam-Lee@users.noreply.github.com>
2021-09-09 17:01:46 +0900

    Delete 드림코딩 클론 directory

--- 드림코딩 클론/js/main.js
+++
@@ -1,8 +0,0 @@
-const toggleButton = document.querySelector('.navbar__toggleButton');
-const menu = document.querySelector('.navbar__menu');
-const icons = document.querySelector('.navbar__icons');
-
-toggleButton.addEventListener('click', ()=>{
-  menu.classList.toggle('active');
-  icons.classList.toggle('active');
-})
